5b6f47d9e076dca1b040983eb3ac04e9ef0d67f4
Sharon Cichelli <girlwritescode@gmail.com>
2015-04-05 13:25:31 -0500

    update script compiler test to match refactor from 0bce2354

--- src/Nautilus.Tests/ScriptCompilerTests.cs
+++ src/Nautilus.Tests/ScriptCompilerTests.cs
@@ -32,11 +32,10 @@ namespace TestToBuild
 	public class BuildInstructions : TaskRunner
 	{
 		private const string _pathToSolution = @""C:\play\nautilus\Nautilus-Build\Hello-Nautilus\src\HelloNautilus.sln"";
-		private const string _outputPath = @""C:\play\nautilus\Nautilus-Build\Hello-Nautilus\Output"";
 
 		public void Default()
 		{
-			CompileSolution(_pathToSolution, _outputPath);
+			CompileSolution(_pathToSolution);
 			RunUnitTests();
 		}
 	}
